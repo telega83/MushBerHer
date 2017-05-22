97daaafc6e20e8e16cc8d59e33bc9a87aeb76405
Oleg <oleg@strawberrymac>
2017-05-23 00:00:10 +0300

    Initial Commit

---
+++ MushBerHer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MushBerHer
+//
+//  Created by Oleg on 23/05/2017.
+//  Copyright © 2017 telega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
